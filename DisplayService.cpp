bb2fab17ae2c2250af92b5c4da828519cbd1cb99
troye <troye@THEODORE>
2026-03-05 12:37:11 -0800

    Add project files.

---
+++ DisplayService.cpp
@@ -0,0 +1,1 @@
+#include "DisplayService.h"
---
+++ Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "TaxRedux.h"
+
+using namespace std;
+
+int main()
+{
+	
+	TaxCalculate UserInfo{};
+	
+	UserInfo.collectName();
+	UserInfo.collectAge();
+	UserInfo.collectIncome();
+	UserInfo.collectFilingStatus();
+	UserInfo.getFederalTaxBracketData();
+	UserInfo.displayTaxInfo();
+
+	
+}
