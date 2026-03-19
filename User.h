7bb0ef311b456009841b20174c825f39f4308665
Riplyron3117 <troy.e.bottleson@gmail.com>
2026-03-18 21:28:51 -0700

    begin User.h gig worker refactor - add income period enum, gig worker fields, getters"

--- User.h
+++ User.h
@@ -6,29 +6,90 @@
 
 class User
 {
+public:
+	enum class IncomePeriod
+	{
+		STOP,
+		DAY,
+		WEEK,
+		MONTH
+	};
+
 private:
 		std::string name_ = "";
+		std::string state_ = "";
+
 		char userDisplayChoice_ = ' ';
-		int age_ = 0;
-		double annualIncome_ = 0.0;
-		int filingStatus_ = 0;
-		double taxableIncome_ = 0.0;
+
+		int age_ = 0,
+		    filingStatus_ = 0,
+		    stopsPerDay_ = 0,
+		    daysPerWeek_ = 0;
+
+		double safeSpendingAmount_ = 0.0,
+			   quarterlyPayment_ = 0.0,
+			   totalTaxOwed_ = 0.0,
+			   stateTax_ = 0.0,
+			   federalIncomeTax_ = 0.0,
+			   federalTaxableIncome_ = 0.0,
+			   seDeduction_ = 0.0,
+			   seTax_ = 0.0,
+			   standardDeduction_ = 0.0,
+			   netSEIncome_ = 0.0,
+			   annualIncome_ = 0.0,
+			   taxableIncome_ = 0.0,
+			   businessExpenses_ = 0.0,
+			   incomeAmount_ = 0.0;
+
+	    IncomePeriod incomeFrequency_ = IncomePeriod::STOP;
+
 
 public:
 	//getters for user data
 	User();
+
 		
 	std::string getName() const { return name_; }
 	 
+	char getUserDisplayChoice() const { return userDisplayChoice_; }
+
 	int getAge() const { return age_; }
 
-	double getAnnualIncome() const { return annualIncome_; }
+	IncomePeriod getIncomeFrequency() const { return incomeFrequency_; }
 
 	int getFilingStatus() const { return filingStatus_; }
 
+	int getStopsPerDay() const { return stopsPerDay_; }
+
+	int getDaysPerWeek() const { return daysPerWeek_; }
+
+	double getAnnualIncome() const { return annualIncome_; }
+
 	double getTaxableIncome() const { return taxableIncome_; }
 
-	char getUserDisplayChoice() const { return userDisplayChoice_; }
+	double getIncomeAmount() const { return incomeAmount_; }
+
+	double getBusinessExpenses() const { return businessExpenses_; }
+
+	double getNetSEIncome() const { return netSEIncome_; }
+
+	double getStandardDeduction() const { return standardDeduction_; }
+
+	double getSeTax() const { return seTax_; }
+
+	double getSeDeduction() const { return seDeduction_; }
+
+	double getFederalTaxableIncome() const { return federalTaxableIncome_; }
+
+	double getFederalIncomeTax() const { return federalIncomeTax_; }
+
+	double getStateTax() const { return stateTax_; }
+
+	double getTotalTaxOwed() const { return totalTaxOwed_; }
+
+	double getQuarterlyPayments() const { return quarterlyPayment_; }
+
+	double getSafeSpendingAmount() const { return safeSpendingAmount_; }
 
 	//setters for user data
 	void setName(const std::string& name) { name_ = name; }
@@ -43,8 +104,13 @@ public:
 
 	void setUserDisplayChoice(char choice) { userDisplayChoice_ = choice; }
 
+	void setIncomeFrequency(IncomePeriod frequency) { incomeFrequency_ = frequency; }
 
+	void setStopsPerDay(int stops) { stopsPerDay_ = stops; }
 
+	void setDaysPerWeek(int days) { daysPerWeek_ = days; }
+
+	void setIncomeAmount(double income) { incomeAmount_ = income; }
 
 
 
