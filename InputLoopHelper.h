4dc07372966f0e0679a5462d3534805e38afb5df
troye <troy.e.bottleson@gmail.com>
2026-04-11 14:42:57 -0700

    template rebuild, didnt move over with new branch. minor.

--- InputLoopHelper.h
+++ InputLoopHelper.h
@@ -1,1 +1,11 @@
 #pragma once
+#include <string_view>
+
+template <typename InputType, typename Reader, typename Validator>
+
+InputType inputLoopLogic(std::string_view prompt, Reader input, Validator validation)
+{
+
+}
+
+
